45d22c8e1adeb5422c7255b20997e0e128a18fac
Alen Duda <alenduda@gmail.com>
2017-11-03 03:31:15 +0100

    Enabled exiting queues

--- app/src/main/java/hr/unipu/duda/justintime/adapters/ReservationAdapter.java
+++ app/src/main/java/hr/unipu/duda/justintime/adapters/ReservationAdapter.java
@@ -3,19 +3,32 @@ package hr.unipu.duda.justintime.adapters;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import hr.unipu.duda.justintime.R;
+import hr.unipu.duda.justintime.activities.ReservationsActivity;
 import hr.unipu.duda.justintime.model.Queue;
+import hr.unipu.duda.justintime.util.UserController;
 
 public class ReservationAdapter extends RecyclerView.Adapter<ReservationAdapter.ViewHolder>{
 
@@ -40,6 +53,47 @@ public class ReservationAdapter extends RecyclerView.Adapter<ReservationAdapter.
         holder.myNumber.setText(String.valueOf(queue.getMyNumber()));
         holder.currentNumber.setText(String.valueOf(queue.getCurrentNumber()));
 
+        holder.exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Potvrda izlaza")
+                        .setMessage("Sigurno želite izaći iz reda?")
+                        .setNegativeButton("Ne", null)
+                        .setPositiveButton("Da", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                exitQueue(queue);
+                            }
+                        }).create().show();
+            }
+        });
+
+    }
+
+    //izlaz iz reda
+    private void exitQueue(Queue queue) {
+        RequestQueue volleyQueue = Volley.newRequestQueue(context);
+        String url = "https://justin-time.herokuapp.com/queue/removeUser/" + queue.getFacility().getId() + "/" + queue.getId();
+        url += "?access_token=" + UserController.getInstance().getToken();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("ExitQueue", "onResponse: " +response);
+                Intent intent = new Intent(context, ReservationsActivity.class);
+                //zamjena za recreate() ReservationsActivity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ExitQueueError", "onResponse: " +error);
+            }
+        });
+
+        volleyQueue.add(request);
     }
 
     @Override
@@ -48,7 +102,7 @@ public class ReservationAdapter extends RecyclerView.Adapter<ReservationAdapter.
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView nameView;
         TextView myNumber;
@@ -69,22 +123,6 @@ public class ReservationAdapter extends RecyclerView.Adapter<ReservationAdapter.
             approximateWait = (TextView) view.findViewById(R.id.tvReservationApproximateWait);
             exitButton = (ImageButton) view.findViewById(R.id.imgExitQueue);
 
-            exitButton.setOnClickListener(this);
-
-        }
-
-        @Override
-        public void onClick(View view) {
-            new AlertDialog.Builder(context)
-                    .setTitle("Potvrda izlaza")
-                    .setMessage("Sigurno želite izaći iz reda?")
-                    .setNegativeButton("Ne", null)
-                    .setPositiveButton("Da", new DialogInterface.OnClickListener() {
-
-                        public void onClick(DialogInterface arg0, int arg1) {
-                            //todo: pošalji API request za izlaz iz reda
-                        }
-                    }).create().show();
         }
     }
 }
